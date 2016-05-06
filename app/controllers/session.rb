44efcdd0cfbd157827fca0bbd7198fe46868b819
Saralis Rivera <saralis.rivera@gmail.com>
2016-05-06 16:52:55 -0400

    Add login erb and session controller

---
+++ app/controllers/session.rb
@@ -0,0 +1,21 @@
+get '/login' do
+ erb :'/login/new'
+end
+
+post '/login' do
+  user = User.find_by(params[:username])
+
+  if user.authenticate(params[:user])
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    @errors = user.errors.full_messages
+    erb :'/login/new'
+  end
+end
+
+get 'logout' do
+  login.clear
+
+  redirect '/'
+end
