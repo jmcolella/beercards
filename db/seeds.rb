15e7904e1004ca36cf9a37a7b3d03fa5922a90e8
Christyn Budzyna & John Colella & Lisa & Saralis Rivera <pair+christynbudzyna.jmcolella.lisabee.sarariv@devbootcamp.com>
2016-05-06 18:49:00 -0400

    add seed file and add correct column to guess table

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Deck.create(name: "Brewery Facts", num_of_cards: 2)
+Card.create(question: "where is the bk brewery located?", answer: "williamsburg", deck_id: 1)
+Card.create(question: "where is other half brewery located?", answer: "carroll gardens", deck_id: 1)
